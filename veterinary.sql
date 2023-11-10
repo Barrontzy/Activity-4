d70c8a8a03a02a4bbfd918378d13b39b30ea452f
= <=>
2023-11-10 11:22:21 +0800

    feat/create table invoices

--- veterinary.sql
+++ veterinary.sql
@@ -36,3 +36,11 @@ CREATE TABLE doctors(
     phone VARCHAR(15),
     email VARCHAR(100),
 );
+
+ CREATE TABLE invoices(
+    invoiceid INT PRIMARY KEY,
+    appointid INT,
+    totalamount NUMERIC (10,2),
+    paymentdate TIME,
+        FOREIGN KEY (appointid) REFERENCES appointments(appointid)
+);
