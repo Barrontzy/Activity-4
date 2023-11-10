b54b510113a821aad22625dd5a5a419908f326b9
= <=>
2023-11-10 11:00:53 +0800

    feat-create table owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE veterinary;
+
+CREATE TABLE owners(
+    ownerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
