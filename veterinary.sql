38878ccbc1a1de72b9794b1aecbde4aa95e484ab
= <--global>
2023-11-10 12:19:07 +0800

    feat/list animal with most appointments

--- veterinary.sql
+++ veterinary.sql
@@ -170,3 +170,11 @@ WHERE animalid IN (
 
 SELECT SUM(totalamount) AS total_sales
 FROM invoices;
+
+SELECT a.animalid, a.name, COUNT(*) AS appointment_count
+FROM animals a
+JOIN appointments ap 
+	ON a.animalid = ap.animalid
+GROUP BY a.animalid, a.name
+ORDER BY appointment_count DESC
+LIMIT 1;
