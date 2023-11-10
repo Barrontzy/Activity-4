c79e04e416ef94710810705644758019c7d9a3ca
= <--global>
2023-11-10 12:11:49 +0800

    feat/Update doctor Gonzales

--- veterinary.sql
+++ veterinary.sql
@@ -148,3 +148,7 @@ DELETE FROM appointments
 WHERE animalid = (
 	SELECT animalid 
 	FROM animals WHERE name = 'Simba');
+
+UPDATE doctors
+SET dlastname = 'Reyes-Gonzales'
+WHERE dfirstname = 'Dr. Sofia';
