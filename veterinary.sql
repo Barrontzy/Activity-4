db93d94d643034fc95b1109e7724d5abc7d69e00
= <--global>
2023-11-10 11:04:07 +0800

    feat-create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -8,3 +8,14 @@ CREATE TABLE owners(
     phone VARCHAR(15),
     email VARCHAR(100)
 );
+CREATE TABLE animals (
+    animalid INT PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    ownerid INT,
+    FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
