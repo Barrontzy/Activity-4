778dcd909d49d3fb11a7145733e955e789157ece
= <=>
2023-11-10 12:10:41 +0800

    feat/delete simba

--- veterinary.sql
+++ veterinary.sql
@@ -135,3 +135,16 @@ ADD COLUMN registereddate DATE;
 
 ALTER TABLE invoices
 RENAME COLUMN paymentdate TO paymenttime;
+
+DELETE FROM invoices
+WHERE appointid = (
+	SELECT appointid 
+	FROM appointments 
+	WHERE animalid = (
+		SELECT animalid 
+		FROM animals WHERE name = 'Simba'));
+
+DELETE FROM appointments
+WHERE animalid = (
+	SELECT animalid 
+	FROM animals WHERE name = 'Simba');
